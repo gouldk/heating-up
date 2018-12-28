264e49ad5eec76e3dd5a22efa7448811d17c5c5a
Kyle <Kyle@kyles-macbook-air.local>
2018-12-28 16:04:50 -0500

    Initial Commit

---
+++ heatingup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  heatingup
+//
+//  Created by Kyle on 12/28/18.
+//  Copyright © 2018 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
